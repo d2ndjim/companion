bea91533007e0406015d715d6cf8345985cc4ed3
Lekan <jimoridwanlekan@gmail.com>
2023-08-17 15:21:34 +0100

    Add search filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { SearchInput } from "@/components/search-input";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+    </div>
+  );
 }
